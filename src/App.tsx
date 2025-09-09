ac75c2c21411a97191b1265784a164189f49457f
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-09-09 11:24:43 -0300

    Construção das Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar />
-      <Home />
-    <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-screen">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
